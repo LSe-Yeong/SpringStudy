3af3a8578cc56680795051141f1af3fb92237d10
Lse-Yeong <ocdee39@gmail.com>
2026-01-24 15:06:48 +0900

    8강 : 테스트 메서드 작성

--- springtdd/src/test/java/com/example/springtdd/integration/PostControllerIntegrationTest.java
+++ springtdd/src/test/java/com/example/springtdd/integration/PostControllerIntegrationTest.java
@@ -1,11 +1,25 @@
 package com.example.springtdd.integration;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.example.springtdd.domain.post.dto.PostRequestDto;
+import com.example.springtdd.domain.post.entity.PostEntity;
+import com.example.springtdd.domain.post.repository.PostRepository;
+
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import tools.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,5 +30,43 @@ public class PostControllerIntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    PostRepository postRepository;
 
+    @Test
+    void post_통합테스트1() throws Exception {
+
+        // given
+        PostRequestDto dto = new PostRequestDto();
+        dto.setTitle("제목");
+        dto.setContent("내용");
+
+        // when & then
+        mockMvc.perform(post("/post")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").exists());
+
+    }
+
+    @Test
+    void get_통합테스트1() throws Exception {
+
+        // given
+        PostEntity savedEntity = new PostEntity();
+        savedEntity.setTitle("제목");
+        savedEntity.setContent("내용");
+        Long savedId = postRepository.save(savedEntity).getId();
+
+        // when & then
+        mockMvc.perform(get("/post/" + savedId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(savedId))
+                .andExpect(jsonPath("$.title").value("제목"))
+                .andExpect(jsonPath("$.content").value("내용"));
+
+    }
 }
