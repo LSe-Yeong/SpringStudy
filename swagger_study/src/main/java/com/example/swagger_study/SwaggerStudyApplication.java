b636a67c9d64ecab53e4417afec0e2a69cb2b8b2
Lse-Yeong <ocdee39@gmail.com>
2026-04-18 16:30:38 +0900

    Swagger 2강 세팅

---
+++ swagger_study/src/main/java/com/example/swagger_study/SwaggerStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.swagger_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerStudyApplication.class, args);
+	}
+
+}
