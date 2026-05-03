9b39c9a56ef91ce8d14aa99cb4545af6abb35641
Lse-Yeong <ocdee39@gmail.com>
2026-05-03 09:25:23 +0900

    5강 : DB 연결 및 User Entity 생성

---
+++ SpringJwt/src/main/java/com/example/SpringJwt/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.SpringJwt.repository;
+
+import com.example.SpringJwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
